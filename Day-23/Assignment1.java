9716b1405811a55dbb9ea06fc2f6157e4ed05643
25116cn027-rudransh-rai <25116cn027@glbitm.ac.in>
2026-06-23 22:27:33 +0530

    Create Day-23/Assignment1.java

---
+++ Day-23/Assignment1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        str = str.toLowerCase();
+
+        char result = '-';
+        for (int i = 0; i < str.length(); i++) {
+            int count = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                result = str.charAt(i);
+                break;
+            }
+        }
+
+        if (result != '-') {
+            System.out.println("First non-repeating character = " + result);
+        } else {
+            System.out.println("No non-repeating character found.");
+        }
+        sc.close();
+    }
+}
